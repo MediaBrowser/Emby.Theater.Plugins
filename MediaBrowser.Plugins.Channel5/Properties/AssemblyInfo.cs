5e707edefe32ebccd0dd37de6d8aa8b24ce6ab04
Stephen Maunder <snazy2000@btinternet.com>
2013-10-18 11:18:16 +0100

    Updated Assembly info

--- MediaBrowser.Plugins.Channel5/Properties/AssemblyInfo.cs
+++ MediaBrowser.Plugins.Channel5/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediaBrowser.Plugins.Steam")]
+[assembly: AssemblyTitle("MediaBrowser.Plugins.Channel5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MediaBrowser.Plugins.Steam")]
+[assembly: AssemblyProduct("MediaBrowser.Plugins.Channel5")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
